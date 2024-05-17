64ea01d8afadd2de17d5028e78de128627fda2ae
ikanx101 <ikanx101@gmail.com>
2024-05-17 04:17:55 +0000

    Save dulu ya

--- 1 agregato.R
+++ 1 agregato.R
@@ -17,6 +17,6 @@ ambil_dat = function(file){
 temp  = mclapply(files,ambil_dat,mc.cores = n_core)
 final = do.call(rbind,temp)
 
-write.csv(final,"~/ARFWSNMRP/Agregat/agregat 17 May 2024.csv")
+write.csv(final,"~/ARFWSNMRP/Agregat/agregat 17 May 2024 new.csv")
 
 unlink(files)
