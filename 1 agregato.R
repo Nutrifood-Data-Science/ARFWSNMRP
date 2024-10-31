a684b9a5ec4a223ad039646a9e9c00fa6bb9b4e7
ikanx101 <ikanx101@gmail.com>
2024-10-31 14:42:09 +0700

    rapihin 31 otk siang

--- 1 agregato.R
+++ 1 agregato.R
@@ -18,6 +18,6 @@ temp  = mclapply(files,ambil_dat,mc.cores = n_core)
 final = data.table::rbindlist(temp) |> as.data.frame()
 # final = do.call(rbind,temp)
 
-write.csv(final,"~/ARFWSNMRP/Agregat/agregat 17 sept 2024.csv")
+write.csv(final,"~/ARFWSNMRP/Agregat/agregat 31 Okt 2024.csv")
 
 unlink(files)
