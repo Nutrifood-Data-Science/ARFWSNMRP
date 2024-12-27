1c5cf6290691428769d7b142af10af4539c164a7
ikanx101 <ikanx101@gmail.com>
2024-12-27 10:15:48 +0700

    new data agregator

--- 1 agregato.R
+++ 1 agregato.R
@@ -18,6 +18,6 @@ temp  = mclapply(files,ambil_dat,mc.cores = n_core)
 final = data.table::rbindlist(temp) |> as.data.frame()
 # final = do.call(rbind,temp)
 
-write.csv(final,"~/ARFWSNMRP/Agregat/agregat 31 Okt 2024.csv")
+write.csv(final,"~/ARFWSNMRP/Agregat/agregat 27 Des 2024.csv")
 
 unlink(files)
