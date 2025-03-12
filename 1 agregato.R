3b12fb941c339592152eac360969927b46228ece
ikanx101 <ikanx101@gmail.com>
2025-03-12 01:21:23 +0000

    agregating 12 maret 2025

--- 1 agregato.R
+++ 1 agregato.R
@@ -18,6 +18,6 @@ temp  = mclapply(files,ambil_dat,mc.cores = n_core)
 final = data.table::rbindlist(temp) |> as.data.frame()
 # final = do.call(rbind,temp)
 
-write.csv(final,"~/ARFWSNMRP/Agregat/agregat 3 Mar 2025.csv")
+write.csv(final,"~/ARFWSNMRP/Agregat/agregat 12 Mar 2025.csv")
 
 unlink(files)
