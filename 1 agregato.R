47eba3f445f810e7eb0871100ebc1a41093c9dc3
ikanx101 <ikanx101@gmail.com>
2025-05-28 03:11:54 +0000

    save dulu rekapan 28 May

--- 1 agregato.R
+++ 1 agregato.R
@@ -18,6 +18,6 @@ temp  = mclapply(files,ambil_dat,mc.cores = n_core)
 final = data.table::rbindlist(temp) |> as.data.frame()
 # final = do.call(rbind,temp)
 
-write.csv(final,"~/ARFWSNMRP/Agregat/agregat 26 May 2025.csv")
+write.csv(final,"~/ARFWSNMRP/Agregat/agregat 28 May 2025.csv")
 
 unlink(files)
